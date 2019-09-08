a161ffea4e83e9cf33ea47b9007a922fd40b90e8
Mateusz Pycka <matpyc2@gmail.com>
2019-09-08 12:47:34 +0200

    finish 'DatePicker' class

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,4 +1,5 @@
-import {select} from '../settings.js';
+/* global flatpickr */
+import {select, settings} from '../settings.js';
 import {utils} from '../utils.js';
 import BaseWidget from './BaseWidget.js';
 
@@ -16,6 +17,43 @@ class DatePicker extends BaseWidget {
   initPlugin() {
     const  thisWidget = this;
     thisWidget.minDate = new Date(thisWidget.value);
+    console.log('minDate:', thisWidget.minDate);
+    thisWidget.maxDate = thisWidget.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+    console.log('maxDate:', thisWidget.maxDate);
+
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      minDate: thisWidget.minDate,
+      maxDate: thisWidget.maxDate,
+      locale: {
+        firstDayOfWeek: 1 // start week on Monday
+      },
+      disable: [
+        function(date) {
+          return (date.getDay() === 1);
+        }
+      ],
+      onChange: function(selectedDays, dateStr) {
+        thisWidget.value = dateStr;
+      }
+    });
+  }
+
+  addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  parseValue(date) {
+    return date;
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
 
   }
 
